59aa6caadab4ecf1e0cfc2f0e4fdd7c49b7827c7
Luis Tome <luisptcolaco@gmail.com>
2020-02-18 09:54:57 +0000

    Probuilder is buggy when separating objects, better to do one room at a time, added door animations

---
+++ PRCO304/Assets/Scripts/EntranceDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceDoor : MonoBehaviour
+{
+
+    public Animator anim;
+    private bool locked = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    void OnMouseDown()
+    {
+
+
+        if (anim.GetBool("open") == false)
+        {
+            anim.SetTrigger("open");
+        }
+        else
+            anim.ResetTrigger("open");
+
+
+
+
+    }
+
+}
