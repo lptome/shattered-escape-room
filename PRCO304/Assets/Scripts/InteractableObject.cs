e425a72e6634ea37dff065401c9a697bfaf1882a
Luis Tome <luisptcolaco@gmail.com>
2020-03-10 15:58:03 +0000

    Tweaked InteractableItem script and changed some models, started working on the living room

--- PRCO304/Assets/Scripts/InteractableObject.cs
+++ PRCO304/Assets/Scripts/InteractableObject.cs
@@ -8,17 +8,25 @@ public class InteractableObject : MonoBehaviour
     [SerializeField] Item itemNeeded;
     [SerializeField] Item itemGiven;
     [SerializeField] Inventory inventory;
-   
+    [SerializeField] SelectedPanel selectedPanel;
+
     public GameObject popupMessage;
-    public GameObject interactableObject;
-    public SelectedPanel selectedPanel;
+    
+    //These are used to store the object appearance before and after an item is used.
+    public GameObject initialObject;        
+    public GameObject finalObject;       
+    
+    
 
     //These two strings can be changed in the editor per necessary, default text displays a description of the object,
     //interactText displays a message once the correct item is used on the object.
     [TextArea] public string defaultText;
     [TextArea] public string interactText;
+
+    //Determines whether object is destroyed once the correct item is used on it.
     public bool destructable;
 
+    //Setting up GUI text.
     private float showText;
     private float textDuration = 3f;
 
@@ -54,7 +62,6 @@ public class InteractableObject : MonoBehaviour
             }
             popupMessage.GetComponent<Text>().text = interactText;
             popupMessage.SetActive(true);
-            Debug.Log("Got here.");
             showText = Time.time + textDuration;
 
             if (itemGiven != null)
@@ -62,13 +69,14 @@ public class InteractableObject : MonoBehaviour
                 inventory.AddItem(itemGiven);
                 selectedPanel.SelectItem(itemGiven);
             }
+
+            finalObject.SetActive(true);
         }
 
         else
         {
             popupMessage.GetComponent<Text>().text = defaultText;
             popupMessage.SetActive(true);
-            Debug.Log("Got here.");
             showText = Time.time + textDuration;
         }
     }
